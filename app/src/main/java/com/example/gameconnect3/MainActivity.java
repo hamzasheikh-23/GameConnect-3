23b52d315b4aca66aab8d913dd9e217b1cb1c369
hamzasheikh-23 <hamzashayk@hotmail.com>
2021-10-08 10:32:48 -0700

    added red

---
+++ app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gameconnect3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn (View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

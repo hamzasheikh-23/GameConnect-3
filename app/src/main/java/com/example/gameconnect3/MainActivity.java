c506bc1b390da3eb09d343676751947340238f86
hamzasheikh-23 <hamzashayk@hotmail.com>
2021-10-08 11:42:42 -0700

    added yellow and tags

--- app/src/main/java/com/example/gameconnect3/MainActivity.java
+++ app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -3,11 +3,37 @@ package com.example.gameconnect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //0:yellow, 1:red, 2:empty
+    int[] gamestate = {2,2,2,2,2,2,2,2,2};
+    int activePlayer = 0;
+
     public void dropIn (View view) {
+
+        ImageView counter = (ImageView) view;
+
+        Log.i("tag", counter.getTag().toString());
+
+        counter.setTranslationY(-1500);
+        counter.setRotation(-7200);
+
+        if (activePlayer == 0) {
+
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+
+        } else {
+
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
+        counter.animate().translationYBy(1500).rotation(360).setDuration(1000);
 
     }
 
